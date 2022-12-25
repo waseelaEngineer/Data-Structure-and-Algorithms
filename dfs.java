41c833d00e0e1389dc0651af8b33887d238af457
Mohamed <d2105944@hotmail.com>
2022-12-25 15:11:01 +0300

    DFS

--- dfs.java
+++ dfs.java
@@ -21,7 +21,7 @@ import java.util.List;
 import javax.swing.tree.TreeNode;
 
 public class dfs {
-
+	// DFS has 3 ways of travarsal, preOrder, inOrder and postOrder. in this example we used inOrder.
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
 		System.out.println(inorderTraversal(root));
@@ -35,6 +35,7 @@ public class dfs {
 
     static void helper(TreeNode root, List<Integer> res) {
         if (root != null) {
+        	//in inOrder traversal we go left then parent then right.
             helper(root.left, res);
             res.add(root.val);
             helper(root.right, res);
